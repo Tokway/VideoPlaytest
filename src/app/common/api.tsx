c06768f449e438fa411bc70dff059ddde3129bda
Tokway <linktokway@gmail.com>
2024-06-23 18:20:42 -0700

    Update api.tsx

--- src/app/common/api.tsx
+++ src/app/common/api.tsx
@@ -1,6 +1,7 @@
 import { VideoComment, Video} from "./types"
 
-const BASE_URL = 'https://take-home-assessment-423502.uc.r.appspot.com/api/openapi.json';
+const BASE_URL = 'https://take-home-assessment-423502.uc.r.appspot.com/api';
+const userId = 'yurii_kravchenko';
 
 export async function getUserVideos(userId: string): Promise<Video[]> {
     const response = await fetch(`${BASE_URL}/videos?user_id=${userId}`);
